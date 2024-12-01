2af396866c34b5c7c9f8bfa96eec84821f07588b
chris thoma <christhoma0@gmail.com>
2024-12-01 07:49:57 -0500

    Ignore

--- src/main/kotlin/ProblemRunner.kt
+++ src/main/kotlin/ProblemRunner.kt
@@ -1,6 +1,7 @@
 import java.io.File
 
 interface ProblemRunner {
+    val filePath: String
     fun read(path: String) : File
     fun answerPartOne(input: File) : String
     fun answerPartTwo(input: File) : String
